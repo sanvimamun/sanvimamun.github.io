1e8c4cbef1a6e3fed858c0c1523954c0097e9048
Md Mamunur Roshid <66670960+sanvimamun@users.noreply.github.com>
2020-07-15 12:37:59 +0600

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -69,14 +69,14 @@ $(document).ready(function () {
   });
   
 	// Isotope js	
-  var $grid = $('.grid').isotope({
-	  itemSelector: '.grid-item',
-	  percentPosition: true,
-	  masonry: {
-		// use outer width of grid-sizer for columnWidth
-		columnWidth: '.grid-sizer'
-	  }
-	})
+	var $grid = $('.grid').isotope({
+		  // options
+		});
+		// filter items on button click
+		$('.filter-button-group').on( 'click', 'button', function() {
+		  var filterValue = $(this).attr('data-filter');
+		  $grid.isotope({ filter: filterValue });
+	});
 	
 	// filter items on button click
 	$('.filter-button-group').on( 'click', 'button', function() {
@@ -99,4 +99,21 @@ $(document).ready(function () {
 		  }
 		});
 	});
+	// wow js
+	var wow = new WOW(
+	  {
+		boxClass:     'wow',      // animated element css class (default is wow)
+		animateClass: 'animated', // animation css class (default is animated)
+		offset:       0,          // distance to the element when triggering the animation (default is 0)
+		mobile:       true,       // trigger animations on mobile devices (default is true)
+		live:         true,       // act on asynchronously loaded content (default is true)
+		callback:     function(box) {
+		  // the callback is fired every time an animation is started
+		  // the argument that is passed in is the DOM node being animated
+		},
+		scrollContainer: null,    // optional scroll container selector, otherwise use window,
+		resetAnimation: true,     // reset animation on end (default is true)
+	  }
+	);
+	wow.init();
 });
